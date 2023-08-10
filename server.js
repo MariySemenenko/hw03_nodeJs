9100393dc9242815d19076d3e118013a23486969
MariySemenenko <mashyni92@gmail.com>
2023-08-10 10:45:42 +0300

    evn

--- server.js
+++ server.js
@@ -7,8 +7,7 @@
 const app = require("./app");
 
 const mongoose = require("mongoose");
-const DB_HOST = 'mongodb+srv://maria:ucKDALy0frstcq5c@cluster0.hmvws2a.mongodb.net/contacts_id?retryWrites=true&w=majority';
-
+const { DB_HOST, PORT = 3000 } = process.env;
 
 mongoose.set('strictQuery', true);
 
@@ -16,9 +15,9 @@ mongoose
   .connect(DB_HOST)
   .then(() => {
     console.log("Database connection successful")
-    app.listen(3000);
+    app.listen(PORT);
   })
   .catch((error) => {
     console.log(`Database connection failed. Error message: ${error.message}`);
-    process.exit(1);//cloused running processes
+    process.exit(1);
   });
